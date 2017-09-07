cdbe9157adb50ffcd0e3a26a053f24ba61016cba
Lyudmyla <kaluzhynova@gmail.com>
2017-09-07 12:09:00 +0300

    first commit

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+from tablib import Dataset
+
+def simple_upload(request):
+    if request.method == 'POST':
+        person_resource = PersonResource()
+        dataset = Dataset()
+        new_persons = request.FILES['myfile']
+
+        imported_data = dataset.load(new_persons.read())
+        result = person_resource.import_data(dataset, dry_run=True)  # Test the data import
+
+        if not result.has_errors():
+            person_resource.import_data(dataset, dry_run=False)  # Actually import now
+
+    return render(request, 'core/simple_upload.html')
